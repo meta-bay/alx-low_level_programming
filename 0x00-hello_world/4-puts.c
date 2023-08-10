c2743ab20cc6a4152118e0a2e1e43de1d99f2e86
meta-bay <bayiemeseret@gmail.com>
2023-08-11 02:36:27 +0300

    Just copied another repo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starting point.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
