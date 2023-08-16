980296631cbe3d9f138545e0dd382aea0c6b6618
meta-bay <bayiemeseret@gmail.com>
2023-08-16 04:57:35 +0300

    huff

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - the sum
+ * @num1: para1
+ * @num2: para2
+ * Return: sum of num1 and num2
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
+
