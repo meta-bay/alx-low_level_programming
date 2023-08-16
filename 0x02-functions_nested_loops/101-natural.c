68061a0f028f13566b387631d976493e779e5cd3
meta-bay <bayiemeseret@gmail.com>
2023-08-16 05:22:18 +0300

    huff

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - sum of multiple of 3 & 5 < 1024
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
