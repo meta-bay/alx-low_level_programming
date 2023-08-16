e4f179903ec4a54830023ce7e49405d6e5cdb10c
meta-bay <bayiemeseret@gmail.com>
2023-08-16 04:52:14 +0300

    huff

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 1; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
