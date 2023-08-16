1610e9c65ea6f21417588191d093726ccfdfea62
meta-bay <bayiemeseret@gmail.com>
2023-08-16 03:11:17 +0300

    ha it worked

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all the alphabets in lowercase
+ */
+
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(letter);
+	_putchar('\n');
+}
+
