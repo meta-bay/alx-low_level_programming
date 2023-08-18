9c9feac6b4f9741529e76bee52e5731bc00ae4d2
meta-bay <bayiemeseret@gmail.com>
2023-08-18 05:53:53 +0300

    nested loops :)

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_most_numbers - except 2 and 4
  *
  * Return: 0
  */
+
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 48; i <= 57; i++)
 	{
 		if ((i == 50) || (i == 52))
@@ -19,3 +21,4 @@ void print_most_numbers(void)
 	}
 	putchar(10);
 }
+
