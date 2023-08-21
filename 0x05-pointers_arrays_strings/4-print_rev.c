40f6bcb2ff50ad1b034fb8c0d4143a6cf29197a0
meta-bay <bayiemeseret@gmail.com>
2023-08-22 02:49:55 +0300

    pointer arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,12 @@
 void print_rev(char *s)
 {
 	int length;
+	int i;
 
 	length = 0;
 	while (s[length] != '\0')
 		length++;
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
