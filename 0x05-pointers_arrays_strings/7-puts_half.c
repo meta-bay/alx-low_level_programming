ae70e60326e6469652bcc2f8679d5f6cba7415f0
meta-bay <bayiemeseret@gmail.com>
2023-08-22 04:45:32 +0300

    pointer arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,17 @@ void puts_half(char *str)
 	int i;
 
 	length = 0;
-	while (s[length] != '\0')
+	while (str[length] != '\0')
 		length++;
 	n = (length / 2);
 
 	if ((length % 2) == 1)
 		n = ((length + 1) / 2);
-
-	for (i = n; str[i] != '\0'; i++)
+	i = n;
+	while (str[i] != '\0')
+	{
 		_putchar(str[i]);
+		i++
+	}
 	_putchar('\n');
 }
