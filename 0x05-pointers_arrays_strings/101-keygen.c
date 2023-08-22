6f295957004c8ce6c4b5155c9b142af1fa228b8c
meta-bay <bayiemeseret@gmail.com>
2023-08-22 05:40:47 +0300

    pointer arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - the main part
+ * Return: 0
+ */
+
+int main(void)
+{
+	int password[1000];
+	int i, num, sum;
+
+	sum = 0;
+	srand(time(NULL));
+	for (i = 0; i < 1000; i++)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[0] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			num = 2772 - sum - '0';
+			sum += num;
+			putchar(num + '0');
+			break;
+		}
+	}
+	return (0);
+}
+
