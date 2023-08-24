35ff47fd6ddf02278bc6ad39863b5510931803b9
meta-bay <bayiemeseret@gmail.com>
2023-08-24 14:14:23 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize the letter of a word
+ * @str: the string
+ * Return: the string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] < 'a')
+			i++;
+		if (str[i--] == " " ||
+			 str[i--] == "\t" ||
+			 str[i--] == "\n" ||
+			 str[i--] == "," ||
+			 str[i--] == ";" ||
+			 str[i--] == "." ||
+			 str[i--] == "!" ||
+			 str[i--] == "?" ||
+			 str[i--] == '"' ||
+			 str[i--] == "(" ||
+			 str[i--] == ")" ||
+			 str[i--] == "{" ||
+			 str[i--] == "}" ||)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
