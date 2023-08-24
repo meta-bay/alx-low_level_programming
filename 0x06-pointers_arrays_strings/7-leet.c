7a0e454027c744288845c33dd45cf5ad8e7abcbd
meta-bay <bayiemeseret@gmail.com>
2023-08-24 19:28:23 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: the string
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int i;
+	char arr1[] = "aAeEoOtTlL";
+	char arr2[] = "4433007711";
+
+	while (*str != '\0')
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (str[i] == arr1[i])
+				str[i] = arr2[i];
+		}
+	}
+	return (str);
+}
+
