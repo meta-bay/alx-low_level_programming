7db2e9cc0e1092310c8de2a8e2dffb67bdbe090e
meta-bay <bayiemeseret@gmail.com>
2023-08-25 13:49:10 +0300

    pointers

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,24 +19,24 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < size; i += 10)
 		{
 			printf("%.8x:", i);
-			for (j = i; j < i + 10; j++)
+			for (j = i; j < (i + 10); j++)
 			{
 				if (j % 2 == 0)
 					printf(" ");
 				if (j < size)
-					printf("%.2x", *(b + j));
+					printf("%.2x", b[j]);
 				else
 					printf(" ");
 			}
 			printf(" ");
-			for (k = i; k < i + 10; k++)
+			for (k = i; k < (i + 10); k++)
 			{
 				if (k >= size)
 					break;
-				if (*(b + k) < 32 || *(b + k) > 126)
+				if (b[k] < 32 || b[k] > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + k));
+					printf("%c", b[k]);
 			}
 			printf("\n");
 		}
