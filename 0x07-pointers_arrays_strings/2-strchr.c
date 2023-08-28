146692adc28d3c292e37d6ce0a79314818a9c051
meta-bay <bayiemeseret@gmail.com>
2023-08-28 16:20:59 +0300

    pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,14 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] <= '\0')
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
 		i++;
 	}
+	if (s[i] == '\0')
+		return (&s[i]);
 	return (0);
 }
 
