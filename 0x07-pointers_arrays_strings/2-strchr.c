f1fdd77a4c3299310cd47b9de3d33b0cdc80821c
meta-bay <bayiemeseret@gmail.com>
2023-08-28 15:59:18 +0300

    pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#define NULL ((void*)0)
+#define NULL ((void *)0)
 
 /**
  * _strchr - finds char c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return (s);
+			return (char *)s;
 		s++;
 	}
 	return (NULL);
