2ae87da176ac25af649f0ec7fc85392ea19fc4f2
meta-bay <bayiemeseret@gmail.com>
2023-08-28 18:24:51 +0300

    pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - conjuction
+ * @s: pointer
+ * @accept: pointer
+ * Return: n_bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int n_byte;
+
+	i = 0;
+	n_byte = 0;
+	while (s[i])
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n_byte++;
+				break;
+			}
+		}
+		i++;
+	}
+	return (n_byte);
+}
+
