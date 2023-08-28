41d1e9f780bd2228db962f54b3b09aeefd994940
meta-bay <bayiemeseret@gmail.com>
2023-08-28 11:47:20 +0300

    more on pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory address
+ * @dest: the destination
+ * @src: The source
+ * @n: number of bytes
+ * Return: returns the destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
