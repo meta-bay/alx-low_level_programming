fda76ab89957cd0c398de77bda6bfc1408d57cfb
meta-bay <bayiemeseret@gmail.com>
2023-09-01 22:08:40 +0300

    the library is closed

---
+++ 0x09-static_libraries/compile_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Compile all .c files in the current directory into separate executables
+
+for source_file in *.c; do
+    object_file="${source_file%.c}.o"  # Generate the corresponding object file name
+    gcc -c "$source_file" -o "$object_file"
+done
