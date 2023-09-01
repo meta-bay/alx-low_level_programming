b2670699b06eff326dd101e922c31620120c00f2
meta-bay <bayiemeseret@gmail.com>
2023-09-02 01:35:35 +0300

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: the array
+ * Return: success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
