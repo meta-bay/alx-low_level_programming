995b62d9838b705b8b80c50a49f7c6e706507b22
meta-bay <bayiemeseret@gmail.com>
2023-09-02 01:56:31 +0300

    argcv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: the array
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			product *= atoi(argv[i]);
+		printf("%d\n", product);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
+
