7a4c986f2d08e75981a9b04b461968e217eee62a
meta-bay <bayiemeseret@gmail.com>
2023-09-02 02:02:44 +0300

    argcv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int i, product = 1;
+	int i;
+	int product;
 
+	product = 1;
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 			product *= atoi(argv[i]);
 		printf("%d\n", product);
-		return (0);
-	} else
-	{
-		printf("Error\n");
-		return (1);
 	}
+	printf("Error\n");
+	return (1);
 }
 
