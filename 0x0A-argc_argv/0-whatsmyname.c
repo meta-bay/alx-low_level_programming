970cf71e93e95f0c5ca23572b14cde5c6346bcbf
meta-bay <bayiemeseret@gmail.com>
2023-09-02 00:53:15 +0300

    fixed the unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,12 @@
  * main - the entry point
  * @argc: count
  * @argv: the array
+ * Return: Success
  */
 
-void main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
 
