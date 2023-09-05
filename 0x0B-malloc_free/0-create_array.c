979bce0de880225c9d2b658498e5648e2a2ac7ca
meta-bay <bayiemeseret@gmail.com>
2023-09-05 14:51:58 +0300

    pointer to the array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * create_array - pointer to the array
+ * @size: the size
+ * @c: character
+ * Return: idk
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size = 0 )
+	{
+		return ('\0');
+	}
+	return (create_array());
+}
+
