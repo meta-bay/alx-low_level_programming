3cc3b19ecc3491b19a2a533258d6d3be5bdf9738
meta-bay <bayiemeseret@gmail.com>
2023-09-05 15:44:46 +0300

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,18 +13,17 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	if (size == 0 )
+	if (size == 0)
 	{
 		return (NULL);
 	}
 	arr = (char *)malloc(size * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
-	       	arr[i] = c;
+		arr[i] = c;
 	}
-
-	    return arr;
+	return (arr);
 }
 
