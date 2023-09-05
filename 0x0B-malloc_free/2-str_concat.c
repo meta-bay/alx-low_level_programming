622f11943b01d7a948ff6c9212a4f169c16c68f3
meta-bay <bayiemeseret@gmail.com>
2023-09-05 21:00:51 +0300

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,16 @@ char *str_concat(char *s1, char *s2)
 	int length1, length2;
 	char *carr;
 
-	while (s1[length1] != '\0')
-		length1++;
-	while (s2[length2] != '\0')
-		length2++;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	while (s1[length1] != '\0')
+		length1++;
+	while (s2[length2] != '\0')
+		length2++;
 
-	carr = malloc((length1 + length2) + 1);
+	carr = malloc(length1 + length2 + 1);
 	if (carr == NULL)
 		return (NULL);
 	i = 0;
