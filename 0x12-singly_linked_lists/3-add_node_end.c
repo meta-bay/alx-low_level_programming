fbbffbf77148aeb42492711222d63c6635224494
meta-bay <bayiemeseret@gmail.com>
2023-09-21 14:48:18 +0300

    singly listed list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds node at the end of the list
+ * @head: pointer to the head pointer
+ * @str: the string
+ * Return: the address of the last element
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr;
+	list_t *endNode = malloc(sizeof(size_t));
+
+	ptr = *head;
+	if (endNode == NULL)
+		return (NULL);
+	endNode->str = strdup(str);
+	endNode->len = strlen(str);
+
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = endNode;
+	return (endNode);
+}
+
