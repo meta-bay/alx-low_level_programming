2aed60505973e6045da0ba732f15fbebb5bcc374
meta-bay <bayiemeseret@gmail.com>
2023-09-21 13:41:58 +0300

    singly listed list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,10 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 	ptr->str = strdup(str);
 	ptr->next = NULL;
 
-	if (NULL)
+	if (ptr == NULL)
 		return (NULL);
+	ptr->len = strlen(str);
 	ptr->next = *head;
 	*head = ptr;
-	return (&ptr);
+	return (ptr);
 }
 
