132d9d0e931ffb5ef050220bbc7062888e8c480e
meta-bay <bayiemeseret@gmail.com>
2023-09-27 05:19:24 +0300

    more singly listed lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a list
+ * @head: the head
+ * @n: data to insert
+ * Return: pointer to the new node, or NULL if it fails
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
+}
