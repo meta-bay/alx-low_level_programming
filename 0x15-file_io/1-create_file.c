709c5f4529842afec475aa53601bf28464c0387e
meta-bay <bayiemeseret@gmail.com>
2023-10-02 15:46:03 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        int fd, to_write, length = 0;
+	 int fd, to_write, length = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (length = 0; text_content[length];)
-                        length++;
-        }
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        to_write = write(fd, text_content, length);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	to_write = write(fd, text_content, length);
 
-        if (fd == -1 || to_write == -1)
-                return (-1);
+	if (fd == -1 || to_write == -1)
+		return (-1);
 
-        close(fd);
+	close(fd);
 
-        return (1);
+	return (1);
 }
+
