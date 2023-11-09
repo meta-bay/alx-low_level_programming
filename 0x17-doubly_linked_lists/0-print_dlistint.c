c2acd6d60c41e9bd6874d44fc373fcd90e0989b2
meta-bay <bayiemeseret@gmail.com>
2023-11-09 17:45:00 +0300

    committing doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that returns no of nodes
+ * @h: the head
+ * Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t no_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		no_of_nodes++;
+	}
+
+	return (no_of_nodes);
+}
