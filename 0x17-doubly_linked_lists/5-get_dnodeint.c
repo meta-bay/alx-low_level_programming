604ecd178e4146bd6eba166a7bd04660d8a05ff3
meta-bay <bayiemeseret@gmail.com>
2023-11-09 21:51:28 +0300

    committing returns node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
 		count++;
 		if (count == index)
 		{
