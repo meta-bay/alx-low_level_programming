2a003d42db6390936fcdee71e271f6e1a33103e2
meta-bay <bayiemeseret@gmail.com>
2023-11-09 22:49:25 +0300

    committing inserting node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,27 +23,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		if (*h != NULL)
-		{
 			(*h)->prev = new_node;
-		}
 		*h = new_node;
 		return (new_node);
 	}
-
 	ptr_to_idx = *h;
 	while (ptr_to_idx != NULL && count < idx)
 	{
 		ptr_to_idx = ptr_to_idx->next;
 		count++;
 	}
-
 	if (count == idx)
 	{
 		new_node->next = ptr_to_idx;
 		new_node->prev = ptr_to_idx->prev;
 		if (ptr_to_idx->prev != NULL)
 		{
+			new_node->prev = ptr_to_idx->prev;
 			ptr_to_idx->prev->next = new_node;
+		} else
+		{
+			new_node->prev = NULL;
+			*h = new_node;
 		}
 		ptr_to_idx->prev = new_node;
 		return (new_node);
