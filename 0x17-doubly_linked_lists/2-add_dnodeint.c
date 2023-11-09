521ed32506e7f3774e76ce6e4b570ff3734d1462
meta-bay <bayiemeseret@gmail.com>
2023-11-09 19:00:11 +0300

    committing adds node at the beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds node at the beginning
+ * @head: the head
+ * @n: the data
+ * Return: the address of the new element, or NULL if it failed
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (*head);
+}
