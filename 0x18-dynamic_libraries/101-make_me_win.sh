e8d55329cc2ec30c4793a0bc2bfde5173d38fcd6
meta-bay <bayiemeseret@gmail.com>
2023-11-13 23:09:45 +0300

    update the script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P ../ https://github.com/meta-bay/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinject.so
-export LD_PRELOAD=LD_PRELOAD=/$PWD/libinject.so ./gm 9 8 10 24 75 9
+export LD_PRELOAD=LD_PRELOAD=/$PWD/libinject.so
