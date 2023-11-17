dd3cfbf588222b9553bd51bd5b789f08abbe4f12
meta-bay <bayiemeseret@gmail.com>
2023-11-17 22:38:29 +0300

    injecting the library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/meta-bay/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinject.so
-export LD_PRELOAD=LD_PRELOAD=/$PWD/libinject.so
+wget -P ../ https://github.com/meta-bay/alx-low_level_programming/blob/main/0x18-dynamic_libraries/injectlib.so
+export LD_PRELOAD=../injectlib.so
