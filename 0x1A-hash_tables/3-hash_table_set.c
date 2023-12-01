03ff9d2ea19880fd0a0e33f999be872fa8a81937
meta-bay <bayiemeseret@gmail.com>
2023-12-01 11:55:43 +0300

    found the memory leak

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,6 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 		{
+			free(ht->array[index]->value);
 			ht->array[index]->value = strdup(value);
 			return (1);
 		}
