63ad7989fefbab3e2d9a1b8e7213514a00d7e024
meta-bay <bayiemeseret@gmail.com>
2023-12-01 08:32:12 +0300

    hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1 if it succeeded, 0 otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			ht->array[index]->value = strdup(value);
+			return (1);
+		}
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
