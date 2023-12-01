46df130ed0646f0f7ebc010883ead29a02730248
meta-bay <bayiemeseret@gmail.com>
2023-12-01 05:52:45 +0300

    fix code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *the_table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
+	if (!size)
+		return (NULL);
 	if (the_table == NULL)
 	{
 		return (NULL);
@@ -20,6 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (the_table->array == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		the_table->array[i] = NULL;
 	}
 	return (the_table);
 }
