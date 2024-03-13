e6b5fef0a6f397d8e1b8ca23886652f6b7313b3b
meta-bay <bayiemeseret@gmail.com>
2024-03-13 18:05:48 +0300

    advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,59 +1,47 @@
 #include "search_algos.h"
 
-int recurse_helper(int *array, size_t left,
-size_t right, int value, ssize_t *match);
-
 /**
- * advanced_binary - search for value in array of sorted ints
- * @array: array to search
- * @size: size of array
- * @value: value to search
- * Return: index of found value; or -1 if not found
+ * advanced_binary_recur - Searches recursively for a value in a sorted
+ * array of integers using binary search.
+ * @array: pointer to the array
+ * @left: the left
+ * @right: the right
+ * @value: The value to search for
+ * Return: index where the value is located.
  */
-int advanced_binary(int *array, size_t size, int value)
-{
-	ssize_t match = -1;
 
-	if (array == NULL)
+int advanced_binary_recur(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (right < left)
 		return (-1);
 
-	return (recurse_helper(array, 0, size - 1, value, &match));
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recur(array, left, i, value));
+	return (advanced_binary_recur(array, i + 1, right, value));
 }
 
 /**
- * recurse_helper - recursive implement of binary search
- * @array: array to search
- * @left: left most index
- * @right: right most index
- * @value: value to search
- * @match: pointer to index of most recent match
- * Return: index of found value; or -1 if not found
+ * advanced_binary - Searches for a value in a sorted array
+ * of integers using advanced binary search.
+ * @array: A pointer to the array
+ * @size: the size
+ * @value: The value to search for
+ * Return: first index where the value is located
  */
-
-int recurse_helper(int *array, size_t left,
-size_t right, int value, ssize_t *match)
+int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i = left, mid;
+	if (array == NULL || size == 0)
+		return (-1);
 
-	if (left > right)
-		return (*match);
-	printf("Searching in array: %d", array[i++]);
-	while (i <= right)
-		printf(", %d", array[i++]);
-	printf("\n");
-
-	mid = left + ((right - left) / 2);
-	if (array[mid] == value)
-	{
-		*match = mid;
-		if (right - left > 1)
-			mid++;
-	}
-	else if (array[mid] < value)
-		return (recurse_helper(array, mid + 1, right, value, match));
-
-	if (mid != 0)
-		return (recurse_helper(array, left, mid - 1, value, match));
-	else
-		return (*match);
+	return (advanced_binary_recur(array, 0, size - 1, value));
 }
